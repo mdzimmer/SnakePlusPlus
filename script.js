eaac0fa68b688c94470ea1c217b80aa6eb813b0d
mdzimmer <matt.dan.zim@gmail.com>
2015-01-27 19:55:40 -0800

    changes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("changes");
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'canvas', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+ 
+function create() {
+	console.log("success");
+}
+ 
+function update() {
+}
